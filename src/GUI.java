947439c529942051dd7384052a82ac80e9d8feb2
timvrakas <tim.vrakas@gmail.com>
2015-01-28 10:39:14 -0600

    Refactored, Cleaned Up GUI.java, set up .gitignore.

--- src/GUI.java
+++ src/GUI.java
@@ -1,3 +1,9 @@
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -5,65 +11,78 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
-public class GUI extends JFrame
-{
+public class GUI extends JFrame {
+
 	private static final long serialVersionUID = 1L;
+
+	// Define initial size of window
 	private static final int WIDTH = 250;
 	private static final int HEIGHT = 100;
-	private JLabel nameL;
-	private JLabel versionL;
-	private JTextField nameF;
-	private JButton undemo;
-	private ChangeButtonHandler Handler;
-	private JComboBox version;
-	private JLabel msg;
-	@SuppressWarnings({ })
-	public GUI()
-	{
-		setTitle("Minecraft UnDemo");
-		Container pane = getContentPane();
-		pane.setLayout(new GridLayout(3, 2));
-		nameL = new JLabel("Enter Username:", SwingConstants.RIGHT);
-		versionL = new JLabel("Select Version:", SwingConstants.RIGHT);
-		msg = new JLabel("");
-		nameF = new JTextField(10);
-		undemo = new JButton("UnDemo!");
-		Handler = new ChangeButtonHandler();
-		undemo.addActionListener(Handler);
-		version = new JComboBox(Main.getVersions());
-		pane.add(nameL);
-		pane.add(nameF);
-		pane.add(versionL);
-		pane.add(version);
-		pane.add(msg);
-		pane.add(undemo);
-		setSize(WIDTH, HEIGHT);
-		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-	}
-	private class ChangeButtonHandler implements ActionListener
-	    {
-	       public void actionPerformed(ActionEvent e)
-	        {
-	    	   if(Main.check((String)version.getSelectedItem(), nameF.getText())){
-	    		   msg.setText("ERROR INVALID");
-	    	   }else{
-	    	   		try {
-						Main.patchJSON((String)version.getSelectedItem(), nameF.getText());
-					} catch (IOException e1) {
-						e1.printStackTrace();
-					}
-	    	   		try {
-						Main.copyJar((String)version.getSelectedItem());
-					} catch (IOException e1) {
-						e1.printStackTrace();
-					}
-	    		   msg.setText("Done!");
-	    	   
-	        }}}}
-	    
 
+	// Define GUI Elements
+	private JLabel infoLabel;
+	private JLabel versionLabel;
+	private JLabel responseMessage;
+	private JTextField usernameField;
+	private JButton unDemoButton;
+	private ButtonListener buttonHandler;
+	private JComboBox<?> installedVersion;
+
+	public GUI() {
+
+		// Setup window
+		setSize(WIDTH, HEIGHT);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle("Minecraft UnDemo");
+
+		// Setup layout
+		Container contentPane = getContentPane();
+		contentPane.setLayout(new GridLayout(3, 2));
+
+		// Initialize elements
+		infoLabel = new JLabel("Enter Username:", SwingConstants.RIGHT);
+		versionLabel = new JLabel("Select Version:", SwingConstants.RIGHT);
+		responseMessage = new JLabel("");
+		usernameField = new JTextField(10);
+		installedVersion = new JComboBox<Object>(Main.getVersions());
+		unDemoButton = new JButton("UnDemo!");
+
+		// Setup button handler
+		buttonHandler = new ButtonListener();
+		unDemoButton.addActionListener(buttonHandler);
+
+		// Add elements to contentPane
+		contentPane.add(infoLabel);
+		contentPane.add(usernameField);
+		contentPane.add(versionLabel);
+		contentPane.add(installedVersion);
+		contentPane.add(responseMessage);
+		contentPane.add(unDemoButton);
+
+		// Activate GUI
+		setVisible(true);
+	}
+
+	private class ButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			// Retrieve data from form
+			String username = usernameField.getText();
+			String version = (String) installedVersion.getSelectedItem();
+			// Check data for validity
+			if (Main.checkInput(version, username)) {
+				// Try to patch and copy jar
+				try {
+					Main.patchJSON(version, username);
+					Main.copyJar(version);
+				} catch (IOException e1) {
+					e1.printStackTrace();
+					responseMessage.setText("Error IOException");
+				}
+			} else {
+				responseMessage.setText("Error Invalid");
+			}
+			responseMessage.setText("Done!");
+
+		}
+	}
+}
